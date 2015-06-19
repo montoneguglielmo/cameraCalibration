ab9e6a7f316592d3102188b094b8eabd17b0fbc2
montoneguglielmo <montone.guglielmo@gmail.com>
2015-06-19 13:17:15 +0200

    Bug correction

--- LoadImages/imgCol2GrayScale.m
+++ LoadImages/imgCol2GrayScale.m
@@ -3,7 +3,7 @@ function grayScaleImg = imgCol2GrayScale(colImage)
 
 grayScaleImg      = cell(size(colImage,1),1);
 
-for indIm = 1:size(colImage,1)-1
+for indIm = 1:size(colImage,1)
     imgTemp                  = squeeze(uint8(colImage(indIm, :, :, :)));
     imgTemp                  = imrotate(imgTemp,180);
     imgTemp                  = double(rgb2gray(imgTemp))./255;
